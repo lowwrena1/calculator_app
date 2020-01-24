b146c4f58e947549ceb9c62d5090abbf0ab84d23
Lorena Ontiveros <lorena0328o@gmail.com>
2020-01-24 08:33:23 -0600

    every button has a data attribute and my javascript has the variables in place from the data attributes.

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]') 
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+//how will i store all the information about what the user inputs into the calculator.
